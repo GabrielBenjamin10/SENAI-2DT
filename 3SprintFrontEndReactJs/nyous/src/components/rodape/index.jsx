a2521455a9b9780d7c6ef2effc3a9bba8202d9e1
Gabriel <gabriel.gaspargbg@gmail.com>
2020-10-23 14:10:41 -0300

    jquery add ao client nyous

---
+++ 3SprintFrontEndReactJs/nyous/src/components/rodape/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rodape = () => {
+    return(
+        <footer className="text-center" style={{marginTop : '70px'}}>
+            <h1>SENAI Informática</h1>
+            <small>Desenvolvido por <a href="https://github.com/fernandoaraujodev">Fernando</a></small>
+        </footer>
+    )
+}
+
+export default Rodape
---
+++ 3SprintFrontEndReactJs/nyous/src/pages/home/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Menu from '../../components/menu';
+import Rodape from '../../components/rodape';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Carousel, Jumbotron, Button, Container, Row, Col, Card} from 'react-bootstrap';
+
+const Home = () => {
+    return(
+        <div>
+            <Menu />
+
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                    className='d-block w-100'
+                    src='https://www.ilhabela.com.br/wp-content/uploads/2016/01/eventos-em-ilhabela.jpg'
+                    alt='First slide'/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className='d-block w-100'
+                    src='https://www.ilhabela.com.br/wp-content/uploads/2016/01/eventos-em-ilhabela.jpg'
+                    alt='First slide'/>
+                </Carousel.Item>
+            </Carousel>
+
+            <Jumbotron className="text-center">
+                <h1>Diversos eventos em um unico local</h1>
+                <p>Encontre eventos nos mais diversos segmentos de forma rápida</p>
+                <p><Button variant="primary" href="/login">Login</Button><Button variant="btn btn-success" style={{marginLeft : '10px'}} href="/cadastrar">Cadastrar</Button></p>
+            </Jumbotron>
+
+            <Rodape />
+        </div>
+    )
+}
+
+export default Home
